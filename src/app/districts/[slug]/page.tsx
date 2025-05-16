f313285b1c2d4cf240d46c2262004204935810b1
nguyenhuulocla2006 <nguyenhuulocla2006@gmail.com>
2025-05-16 21:25:10 +0700

    async&wait

--- src/app/districts/[slug]/page.tsx
+++ src/app/districts/[slug]/page.tsx
@@ -1,16 +1,12 @@
-// import { districtCoordinates } from '@/lib/districtCoordinates';
-// import DistrictCard from '@/components/districts/DistrictCard';
 type PageProps = {
-    params: {
-        slug: string;
-    };
+  params: {
+    slug: string;
+  };
 };
-export default function DetailPage({ params }: PageProps) {
-    const { slug } = params;
-    // const coords = districtCoordinates[slug];
-    return (
-        <div>
-            {slug}
-        </div>
-    );
+
+export default async function DetailPage({ params }: PageProps) {
+    const { slug } = await params;
+    // console.log(params)
+
+    return <div>{ slug}</div>;
 }
